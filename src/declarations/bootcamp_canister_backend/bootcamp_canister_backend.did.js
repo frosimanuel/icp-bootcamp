9a722d08c87d941b85ae656cc987688cb91d07c7
Manu <frosimanuel@gmail.com>
2024-07-25 11:35:50 -0300

    icp_bootcamp

---
+++ src/declarations/bootcamp_canister_backend/bootcamp_canister_backend.did.js
@@ -0,0 +1,11 @@
+export const idlFactory = ({ IDL }) => {
+  return IDL.Service({
+    'addGoal' : IDL.Func([IDL.Text], [], []),
+    'getGoals' : IDL.Func([], [IDL.Vec(IDL.Text)], ['query']),
+    'getManifesto' : IDL.Func([], [IDL.Text], ['query']),
+    'getName' : IDL.Func([], [IDL.Text], ['query']),
+    'greet' : IDL.Func([IDL.Text], [IDL.Text], ['query']),
+    'setManifesto' : IDL.Func([IDL.Text], [], []),
+  });
+};
+export const init = ({ IDL }) => { return []; };
